8df84423c09b01b219b200a0f0cbd0891a144be4
Srilochan <srilochan7@gmail.com>
2025-08-23 00:13:43 +0530

    disaster

--- llm_service.py
+++ llm_service.py
@@ -1,2 +1,10 @@
-def get_llm_advice(text):
-    return f"⚠️ (Demo) I heard you say: {text}. Please consult a doctor for real advice."
+import requests
+
+def get_llm_advice(user_text):
+    prompt = """
+    Act as an ai health assistant and assist the user \n:
+    {user_text}
+    Give advices and warn of symptoms are severe and dangeous.
+    """
+    
+    
